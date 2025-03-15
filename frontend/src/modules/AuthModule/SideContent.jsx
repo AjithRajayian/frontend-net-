e4a0660248058fef6a5f47f70d40f06b4e2d8e0c
AjithRajayian <ajithrajayyan99@gmail.com>
2025-03-15 13:53:21 +0530

    SideContent.jsx

--- frontend/src/modules/AuthModule/SideContent.jsx
+++ frontend/src/modules/AuthModule/SideContent.jsx
@@ -1,5 +1,53 @@
+// import { Space, Layout, Divider, Typography } from 'antd';
+// import logo from '@/style/images/glooft-logo.jpeg';
+// import useLanguage from '@/locale/useLanguage';
+// import { useSelector } from 'react-redux';
+
+// const { Content } = Layout;
+// const { Title, Text } = Typography;
+
+// export default function SideContent() {
+//   const translate = useLanguage();
+
+//   return (
+//     <Content
+//       style={{
+//         padding: '150px 30px 30px',
+//         width: '100%',
+//         maxWidth: '450px',
+//         margin: '0 auto',
+//       }}
+//       className="sideContent"
+//     >
+//       <div style={{ width: '100%' }}>
+//         <img
+//           src={logo}
+//           alt="GLOOFT ERP CRM"
+//           // style={{ margin: '0 0 40px', display: 'block' }}
+//           height={200}
+//           width={200}
+          
+//         />
+
+//         <Title level={1} style={{ fontSize: 28, }}>
+//          GLOOFT ERP / CRM
+//         </Title>
+//         <Text>
+//           Accounting / Invoicing / Quote App <b /> based on Node.js React.js Ant Design
+//         </Text>
+
+//         <div className="space20"></div>
+//       </div>
+//     </Content>
+//   );
+// }
+
+
+
+
+
 import { Space, Layout, Divider, Typography } from 'antd';
-import logo from '@/style/images/idurar-crm-erp.svg';
+import logo from '@/style/images/glooft-logo.jpeg';
 import useLanguage from '@/locale/useLanguage';
 import { useSelector } from 'react-redux';
 
@@ -19,18 +67,11 @@ export default function SideContent() {
       }}
       className="sideContent"
     >
-      <div style={{ width: '100%' }}>
-        <img
-          src={logo}
-          alt="IDURAR ERP CRM"
-          style={{ margin: '0 0 40px', display: 'block' }}
-          height={63}
-          width={220}
-        />
-
+      <div style={{ width: '100%', textAlign: 'center' }}>
         <Title level={1} style={{ fontSize: 28 }}>
-          Free Open Source ERP / CRM
+          <span style={{ color: 'orange' }}>GLOOFT</span> ERP / CRM
         </Title>
+
         <Text>
           Accounting / Invoicing / Quote App <b /> based on Node.js React.js Ant Design
         </Text>
